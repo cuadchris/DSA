8e1e61e2936de202527ef10c28c9556d5df4c233
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-04-05 19:30:01 -0400

    traversal

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -32,6 +32,53 @@ class BinaryTree:
         else:
             print('No duplicates allowed in tree')
 
+    def traverse_tree(self, traversal_type):
+        if traversal_type == 'preorder':
+            return self.preorder_traversal(self.root, '').strip('-')
+        elif traversal_type == 'inorder':
+            return self.inorder_traversal(self.root, '').strip('-')
+        elif traversal_type == 'postorder':
+            return self.postorder_traversal(self.root, '').strip('-')
+        else:
+            print('invalid traversal type')
+
+    # pre-order traversal.
+    # check if current node is null
+    # display node data
+    # recursively traverse left subtree with pre-order method
+    # recursively traverse right subtree with pre-order method
+    def preorder_traversal(self, start, traversal):
+        """Root -> Left -> Right"""
+        if start:
+            traversal += (str(start.data) + '-')
+            traversal = self.preorder_traversal(start.left, traversal)
+            traversal = self.preorder_traversal(start.right, traversal)
+        return traversal
+
+    # check if current node is null
+    # recursively traverse left subtree with in-order method
+    # display node data
+    # recursively traverse right subtree with in-order method
+    def inorder_traversal(self, start, traversal):
+        """Left -> Root -> Right"""
+        if start:
+            traversal = self.inorder_traversal(start.left, traversal)
+            traversal += (str(start.data) + '-')
+            traversal = self.inorder_traversal(start.right, traversal)
+        return traversal
+
+    # check if current node is null
+    # recursively traverse left subtree with post-order method
+    # recursively traverse right subtree with post-order method
+    # display node data
+    def postorder_traversal(self, start, traversal):
+        """Left -> Right -> Root"""
+        if start:
+            traversal = self.postorder_traversal(start.left, traversal)
+            traversal = self.postorder_traversal(start.right, traversal)
+            traversal += (str(start.data) + '-')
+        return traversal
+
 
 tree = BinaryTree()
 tree.insert(8)
@@ -39,8 +86,7 @@ tree.insert(9)
 tree.insert(4)
 tree.insert(2)
 tree.insert(10)
-print(tree.root.data)
-print(tree.root.left.data)
-print(tree.root.left.left.data)
-print(tree.root.right.data)
-print(tree.root.right.right.data)
+tree.insert(6)
+print(tree.traverse_tree('inorder'))  # 2-4-6-8-9-10
+print(tree.traverse_tree('preorder'))  # 8-4-2-6-9-10
+print(tree.traverse_tree('postorder'))  # 2-6-4-10-9-8
